6d9154b4d72eed3b55448abca9850decf7b6d127
Ramon Sanchez <songwright66@gmail.com>
2019-06-04 13:22:14 -0700

    feat: Add login function

--- scripts/auth.js
+++ scripts/auth.js
@@ -23,3 +23,21 @@ logout.addEventListener('click', (e) => {
     console.log('user signed out');
   });
 })
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred.user);
+    // Close the login modal and reset the form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  })
+})
