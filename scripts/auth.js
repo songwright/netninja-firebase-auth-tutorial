b8b0baed6c49ac3637778eecbef7d2d749b57f9a
Ramon Sanchez <songwright66@gmail.com>
2019-06-04 10:57:40 -0700

    feat: Add user logout function

--- scripts/auth.js
+++ scripts/auth.js
@@ -9,9 +9,17 @@ signupForm.addEventListener('submit', (e) => {
 
   // Sign up the user. This is an asynchronous task.
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred);
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
     signupForm.reset();
-  })
+  });
+});
+
+// Logout, an asynchronous function
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user signed out');
+  });
 })
