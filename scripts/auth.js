15e814d9bfa54dd16a929751cadadd66b8cec18a
Ramon Sanchez <songwright66@gmail.com>
2019-06-05 13:01:18 -0700

    feat: Add create new guide function

--- scripts/auth.js
+++ scripts/auth.js
@@ -11,6 +11,22 @@ auth.onAuthStateChanged(user => {
   }
 });
 
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+  e.preventDefault(); // Prevent the page from refreshing.
+
+  db.collection('guides').add({
+    title: createForm['title'].value,
+    content: createForm['content'].value
+  }).then(() => {
+    // close the modal and reset form
+    const modal = document.querySelector('#modal-create');
+    M.Modal.getInstance(modal).close();
+    createForm.reset();
+  })
+})
+
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
