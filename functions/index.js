ed57eedbe1adfad0c9f022102822ba560afdab9a
Ramon Sanchez <songwright66@gmail.com>
2019-06-10 20:46:13 -0700

    feat: Add user admin claim function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,21 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions'); // node environment use require
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addAdminRole = functions.https.onCall((data, context) => {
+  // get user and add custom claim (admin)
+  return admin.auth().getUserByEmail(data.email).then(user => {
+    return admin.auth().setCustomUserClaims(user.uid, {
+      admin: true // admin claim set to "true"
+    });
+  }).then(() => {
+    return {
+      message: `Success! ${data.email} has been made an admin.`
+    }
+  }).catch(err => {
+    return err;
+  });
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
